5036ec460ce758dcb79b71f08a90c2b090d4d7f9
rafapaezbas <rpaezbas@rpaezbas.com>
2022-08-28 20:49:13 +0200

    corrected args

--- app/Main.hs
+++ app/Main.hs
@@ -13,22 +13,23 @@ version = infoOption "0.1" (long "version" <> help "Show version")
 opts :: Parser Opts
 opts = Opts <$> strOption (long "file-path" <> help "Template path.")
 
-done_message :: String
-done_message = "Done!"
-
 checkFile :: String -> IO (Either SomeException String)
 checkFile path = (try $ readFile path) :: IO (Either SomeException String)
 
 checkFolder :: String -> IO (Either SomeException [String])
-checkFolder path = try $ getDirectoryContents (takeDirectory path)
+checkFolder path = try $ getDirectoryContents path
 
-replace_tags :: String -> [String] -> String -> IO()
+replace_tags :: String -> [String] -> String -> IO(String)
 replace_tags path (x:xs) template = do
-  content <- readFile (joinPath[(takeDirectory path), x])
-  replace_tags path xs $ replace_tag template content (['{'] ++ x ++ ['}'])
-replace_tags path [] template = do
-  putStrLn done_message
-  writeFile (path ++ "_") template
+  isDir <- (checkFolder $ (joinPath [(takeDirectory path), x]))
+  case isDir of
+    Right _ -> do
+      replace_tags path xs template
+    Left _ -> do
+      content <- readFile (joinPath[(takeDirectory path), x])
+      replace_tags path xs $ replace_tag template content (['{'] ++ x ++ ['}'])
+
+replace_tags _ [] template = return template
 
 replace_tag :: String -> String -> String -> String
 replace_tag "" _ _ = ""
@@ -39,8 +40,10 @@ main :: IO ()
 main = do
   options <- execParser optsParser
   content <- checkFile $ opt_path options
-  files <- checkFolder $ (takeDirectory $ opt_path options)
+  files <- checkFolder $ takeDirectory $ opt_path options
   case (content, files) of
     ( Left _ , _ ) -> putStrLn "File not found"
     ( _ , Left _ ) -> putStrLn "Folder not found"
-    (Right a, Right b) -> replace_tags (opt_path options) ["file2.txt", "file3.txt"]  a
+    (Right a, Right b) -> do
+      t <- replace_tags (opt_path options) b a
+      writeFile ((opt_path options) ++ "_") t
